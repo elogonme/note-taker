2869bfaef02165972fae30712438f396723b8180
Eldar <elogon@gmail.com>
2020-12-12 01:43:59 -0500

    Implement POST and DELETE api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+const Note = require('./lib/note');
 
 // Set up Express App
 const app = express();
@@ -17,16 +18,33 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.
 
 app.get('/api/notes', (req, res) => {
     notes = readDB();
-    console.log(notes);
     res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
     notes = readDB();
-    const newNote = req.body;
-    console.log('new Note:', newNote)
-    console.log(notes);
-    res.json(notes);
+    const newNote = new Note(req.body.title, req.body.text);
+    notes.push(newNote);
+    if (saveDB(notes)) {
+        res.status(201).json(newNote);
+        console.log('Added new Note:', newNote)
+    } else {
+        res.status(500).send('Could not add Note'); // If file was not able to be saved return error
+    }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    notes = readDB();
+    const indexOfNote = notes.findIndex(note => note.id = id);
+    if (indexOfNote !== -1) {
+        console.log('Deleted Note: ', notes[indexOfNote]);
+        notes.splice(indexOfNote, 1);
+        saveDB(notes);
+        res.sendStatus(204);
+    } else {
+        res.status(404).send();
+    }
 });
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
@@ -37,5 +55,24 @@ app.listen(PORT, () => {
 
 // Helper functions
 const readDB = () => {
-    return JSON.parse(fs.readFileSync(path.join(__dirname, 'db/db.json'), 'utf-8'));
+    try { 
+        const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db/db.json'), {encoding:'utf8'})); 
+        console.log("Successfully read notes from db.js ..."); 
+        console.log(notes);
+        return notes;
+      } catch(err) { 
+        console.error(err); 
+      } 
+}
+
+
+const saveDB = (notes) => {
+    try { 
+        fs.writeFileSync(path.join(__dirname, 'db/db.json'), JSON.stringify(notes));
+        console.log("Successfully saved notes to db.js ..."); 
+        return true;
+      } catch(err) { 
+        console.error(err);
+      } 
+    return false;
 }
